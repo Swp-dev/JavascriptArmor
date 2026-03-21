cb44449c709e881e09931406e01f779ed3caa61c
sWp#2579 <nguha2511@gmail.com>
2026-03-21 11:54:05 +0700

    Update user and version information in watermark.js

--- utils/watermark.js
+++ utils/watermark.js
@@ -9,8 +9,8 @@ return `
 
 __OWN__ = "_kingktn - Trương Nhật Bảo Nam"
 __OBF__ = "JSarmor"
-__USR__ = "${user}"
-__VER__ = "1.7.0"
+__USR__ = "${user} - Requests Protect"
+__VER__ = "2.0.0"
 __DATE__ = "${date}"
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
