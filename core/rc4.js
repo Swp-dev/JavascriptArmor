c63484175971840af01118c92c63680397eb9e5a
sWp#2579 <nguha2511@gmail.com>
2026-03-21 11:49:09 +0700

    Improve rc4 function with key check and optimizations
    Refactor rc4 function to include key validation and use bitwise AND for byte operations.

--- core/rc4.js
+++ core/rc4.js
@@ -1,42 +1,33 @@
 export function rc4(key, str){
 
-let s=[]
-let j=0
-let x
-let res=""
+if(!key) throw new Error("RC4 key empty")
+
+let s=[],j=0,x,res=""
+
+for(let i=0;i<256;i++) s[i]=i
 
 for(let i=0;i<256;i++){
-s[i]=i
-}
-
-for(let i=0;i<256;i++){
-
-j=(j+s[i]+key.charCodeAt(i%key.length))%256
-
-x=s[i]
-s[i]=s[j]
-s[j]=x
-
+  j=(j+s[i]+key.charCodeAt(i%key.length))&255
+  x=s[i]
+  s[i]=s[j]
+  s[j]=x
 }
 
 let i=0
 j=0
 
 for(let y=0;y<str.length;y++){
+  i=(i+1)&255
+  j=(j+s[i])&255
 
-i=(i+1)%256
-j=(j+s[i])%256
+  x=s[i]
+  s[i]=s[j]
+  s[j]=x
 
-x=s[i]
-s[i]=s[j]
-s[j]=x
+  let k=s[(s[i]+s[j])&255]
 
-let k=s[(s[i]+s[j])%256]
-
-res+=String.fromCharCode(str.charCodeAt(y)^k)
-
+  res += String.fromCharCode(str.charCodeAt(y) ^ k)
 }
 
 return res
-
 }
